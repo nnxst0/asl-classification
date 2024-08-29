13e0e62e0b2f9e62ab490db40152c646084cb2e4
nnxst0 <151302570+nnxst0@users.noreply.github.com>
2024-08-29 20:26:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
 from prediction import pred_class
+import torch
 import numpy as np
 
 # Set title 
@@ -10,7 +11,8 @@ st.title('American Sign Language Classification')
 st.header('Please upload a picture')
 
 # Load Model 
-model = tf.keras.models.load_model('asl_model_fold0.h5')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = torch.load('asl_model_fold0.h5', map_location=device)
 
 # Display image & Prediction  
 uploaded_image = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'])
@@ -23,8 +25,11 @@ if uploaded_image is not None:
 
     if st.button('Prediction'):
         # Prediction class
-        label, probli = pred_class(image, model)
+        probli = pred_class(model, image, class_name)
 
         st.write("## Prediction Result")
-        st.write(f"## <span style='color:blue'>{label} : {probli[0][np.argmax(probli[0])]*100:.2f}%</span>",
-                 unsafe_allow_html=True)
+        max_index = np.argmax(probli[0])
+
+        for i in range(len(class_name)):
+            color = "blue" if i == max_index else None
+            st.write(f"## <span style='color:{color}'>{class_name[i]} : {probli[0][i]*100:.2f}%</span>", unsafe_allow_html=True)
