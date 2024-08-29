0e0ee401cb4ba9071c7b3090bb44653f9808409d
nnxst0 <151302570+nnxst0@users.noreply.github.com>
2024-08-29 19:00:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image', use_column_width=True)
     st.write("Classifying...")
-    label, prob = classify_image(image, '/content/drive/MyDrive/asl_model.h5')
+    label, prob = classify_image(image, 'asl_model.h5')
     
     # แสดงผลลัพธ์
     classes = ['A', 'B', 'C', 'D']
